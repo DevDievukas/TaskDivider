8d8678faccaba3805f3cb59560bd4d150a571d13
Norbertas Murauskas <nmurauskasdarbo@gmail.com>
2021-03-19 18:22:00 +0100

    Exctracted routes from app component

---
+++ src/Routes/Routes.js
@@ -0,0 +1,93 @@
+
+import { useSelector }  from 'react-redux'
+import {
+  Route,
+  Switch,
+  Redirect,
+}                       from 'react-router-dom'
+import React            from 'react'
+
+
+import Announcements    from '../Announcements/Annauncements'
+import Auth             from '../Auth/Auth'
+import Houses           from '../House/Houses'
+import Info             from '../Info/Info'
+import People           from '../People/People'
+import Rooms            from '../Rooms/Rooms'
+import HouseNavbar      from '../shared/Navbar/HouseNavbar'
+import Schedule         from '../Schedule/Schedule'
+import SharedItems      from '../SharedItems/SharedItems'
+
+const Routes = () => {
+  const { userId, houseId} = useSelector((state) => ({
+    ...state.auth,
+  }))
+  let routes;
+  if (userId) {
+    routes = (
+      <Switch>
+        <Route path="/:houseId/Schedule" exact>
+          <HouseNavbar />
+          <Schedule />
+        </Route>
+        <Route path="/" exact>
+          <Houses />
+        </Route>
+        <Route path="/:houseId/announcements" exact>
+          <HouseNavbar />
+          <Announcements />
+        </Route>
+        <Route path="/:houseId/shareditems" exact>
+          <HouseNavbar />
+          <SharedItems />
+        </Route>
+        <Route path="/:houseId/rooms" exact>
+          <HouseNavbar />
+          <Rooms />
+        </Route>
+        <Route path="/:houseId/people" exact>
+          <HouseNavbar />
+          <People />
+        </Route>
+        <Route path="/:houseId/info" exact>
+          <HouseNavbar />
+          <Info />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    )
+  } else if (houseId) {
+    routes = (
+      <Switch>
+        <Route path="/schedule" exact>
+          <Schedule />
+        </Route>
+        <Route path="/" exact>
+          <Announcements />
+        </Route>
+        <Route path="/shareditems" exact>
+          <SharedItems />
+        </Route>
+        <Route path="/rooms" exact>
+          <Rooms />
+        </Route>
+        <Route path="/people" exact>
+          <People />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    )
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Auth />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    )
+  }
+  return routes
+}
+
+export default Routes;
