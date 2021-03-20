cae2af0034b4a0fc429b36f79ccdf776b11ae8b2
Norbertas Murauskas <nmurauskasdarbo@gmail.com>
2021-03-20 12:42:28 +0100

    extracted styled components

---
+++ src/Info/styled.js
@@ -0,0 +1,20 @@
+
+import styled 								from 'styled-components'
+
+export const Main = styled.div`
+width: 80%;
+margin: auto;
+margin-top: 1.2em;
+`
+
+export const Inner = styled.div`
+display: flex;
+justify-content: space-between;
+width: 100%;
+margin: auto;
+`
+
+export const HouseName = styled.h4`
+text-align: center;
+margin-bottom: 1.2em;
+`
