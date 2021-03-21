0e6970c6e6dc2537889cd31a0a35adef29c72e65
Norbertas Murauskas <nmurauskasdarbo@gmail.com>
2021-03-21 14:22:21 +0100

    houses form and houseList extraction

--- src/House/Houses.js
+++ src/House/Houses.js
@@ -1,23 +1,25 @@
-import { Form,
-  Formik
-}											 	from 'formik'
-import  { useState } 		from 'react'
-import { useSelector } 	from 'react-redux'
+import { 
+  useSelector,
+  useDispatch,
+}                      	from 'react-redux'
 import React 						from 'react'
 
-import Input 						from '../shared/FormElements/Input'
+import {
+  closeForm,
+  createForm
+}                       from '../Form/thunks'
 import Button 					from '../shared/FormElements/Button'
-import useDeleteData 		from '../shared/hooks/deleteData-hook'
 import useFetchData 		from '../shared/hooks/fetchData-hook'
 import usePostData 			from '../shared/hooks/postData-hook'
-import FormModal 				from '../shared/UIElements/FormModal/FormModal'
 
-import HouseCard 				from './HouseCard'
+import HouseList        from './HouseList'
+import HouseForm        from './form'
 import styles 					from './Houses.module.css'
+
 
 const Houses = () => {
   const { token, userId } = useSelector((state) => ({ ...state.auth }))
-  const [houseCreation, setHouseCreation] = useState(false)
+  const dispatch = useDispatch()
   const loadedData = useFetchData(
     `${process.env.REACT_APP_BACKEND_URL}/house/user/${userId}`,
     {
@@ -27,44 +29,7 @@ const Houses = () => {
     }
   )
   const { post } = usePostData()
-  const { deleteData } = useDeleteData()
 
-  const deleteHouseHandler = (id) => {
-    const deleteFilter = () => {
-      loadedData.setData((prevData) =>
-        prevData.filter((element) => element._id !== id)
-      )
-    }
-    deleteData(
-      `${process.env.REACT_APP_BACKEND_URL}/house/`,
-      {
-        headers: {
-          authorization: `Bearer ${token}`,
-        },
-      },
-      id,
-      deleteFilter
-    )
-  }
-
-  let houses
-
-  if (loadedData.dataLoaded) {
-    if (loadedData.data.length > 0) {
-      houses = loadedData.data.map((house) => {
-        return (
-          <HouseCard
-            houseName={house.houseName}
-            key={house._id}
-            houseId={house._id}
-            deleteHouse={deleteHouseHandler}
-          />
-        )
-      })
-    } else {
-      houses = <h2>There are no houses. Would you like to create one?</h2>
-    }
-  }
 
   const createHouseHandler = (houseName, password) => {
     const addFilter = (res) => {
@@ -89,63 +54,21 @@ const Houses = () => {
       createdHouse,
       addFilter
     )
-    setHouseCreation(false)
+    dispatch(closeForm())
   }
 
-  const form = (
-    <Formik
-      initialValues={{
-        houseName: '',
-        password: '',
-      }}
-      onSubmit={async (values) => {
-        // console.log(values);
-        createHouseHandler(values.houseName, values.password)
-      }}
-    >
-      {() => (
-        <Form className={styles.form}>
-          <Input
-            id='houseName'
-            name='houseName'
-            type='input'
-            title='NAME OF THE HOUSE'
-          />
-          <Input
-            id='password'
-            name='password'
-            type='password'
-            title='EMAIL-PASSWORD'
-          />
-          <div className={styles.buttonsDiv}>
-            <Button
-              type='button'
-              onClick={() => setHouseCreation(false)}
-              cancel
-              className={styles.button}
-            >
-							CANCEL
-            </Button>
-            <Button type='submit' className={styles.button}>
-							ADD!
-            </Button>
-          </div>
-        </Form>
-      )}
-    </Formik>
-  )
+  const callForm = () => {
+    dispatch(createForm(
+      HouseForm(createHouseHandler),
+      'CREATE HOUSE?'
+    ))
+  }
 
   return (
     <div className={styles.housesDiv}>
-      <FormModal
-        onCancel={() => setHouseCreation(false)}
-        header='CREATE HOUSE?'
-        show={houseCreation}
-        form={form}
-      />
-      {houses}
+      <HouseList loadedData={loadedData}/>
       <Button
-        onClick={() => setHouseCreation(true)}
+        onClick={callForm}
         className={styles.createHouseBtn}
         danger
       >
---
+++ src/House/form.js
@@ -0,0 +1,48 @@
+
+import Input 						from '../shared/FormElements/Input'
+import Button 					from '../shared/FormElements/Button'
+import { Form,
+  Formik
+}											 	from 'formik'
+import styles 					from './Houses.module.css'
+import React 						from 'react'
+
+const HouseForm = (props) => {
+
+  return (
+    <Formik
+      initialValues={{
+        houseName: '',
+        password: '',
+      }}
+      onSubmit={async (values) => {
+        // console.log(values);
+        props.createHouseHandler(values.houseName, values.password)
+      }}
+    >
+      {() => (
+        <Form className={styles.form}>
+          <Input
+            id='houseName'
+            name='houseName'
+            type='input'
+            title='NAME OF THE HOUSE'
+          />
+          <Input
+            id='password'
+            name='password'
+            type='password'
+            title='EMAIL-PASSWORD'
+          />
+          <div className={styles.buttonsDiv}>
+            <Button type='submit' className={styles.button}>
+							ADD!
+            </Button>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  )
+}
+
+export default HouseForm
